7d34bb91cfd65024a9884b3004ffb1e450e42ecf
Denis Ricard <d.ricard@me.com>
2016-03-11 13:45:18 -0500

    Implemented basic checks prior to processing login (both email and password present and valid email)

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -27,7 +27,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     override func viewWillAppear(animated: Bool) {
-        loginButton.enabled = false
+        loginButton.enabled = true
         subscribeToKeyboardShowNotifications()
     }
     
@@ -52,18 +52,57 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    
     // ACTIONS FROM THE INTERFACE
     
     @IBAction func loginPressed(sender: AnyObject) {
+        resignIfFirstResponder(userEmail)
+        resignIfFirstResponder(userPassword)
+        
+        if userEmail.text!.isEmpty || userPassword.text!.isEmpty {
+            presentAlertMessage("Missing credential", message: "Please enter both an email address and a password")
+        } else {
+            if !isEmailValid(userEmail.text!) {
+                presentAlertMessage("Invalid email address", message: "Please enter a valid email address")
+            } else {
+                print("process login flow")
+            }
+        }
     }
 
     @IBAction func signUpPressed(sender: AnyObject) {
     }
     
+    // UTILITIES FUNCTIONS
+    
+    func resignIfFirstResponder(textField: UITextField) {
+        if textField.isFirstResponder() {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    func presentAlertMessage(title: String, message: String) {
+        let controller = UIAlertController()
+        controller.title = title
+        controller.message = message
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: { action in self.dismissViewControllerAnimated(true, completion: nil ) })
+        controller.addAction(okAction)
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    
+    func isEmailValid(email: String) -> Bool {
+        
+        let filterString = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", filterString)
+        
+        return emailTest.evaluateWithObject(email)
+        
+    }
     
     // KEYBOARD FUNCTIONS
-    
+
     func keyboardWillShow(notification: NSNotification) {
         //get screen height
         let screenSize: CGRect = UIScreen.mainScreen().bounds
