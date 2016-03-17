79480cd0c650aec868ef65802c1df743902473aa
Denis Ricard <d.ricard@me.com>
2016-03-17 13:00:33 -0400

    added code for wide screen for UILabels in LocationViewController and refactored some. Made all labels their own width.

--- OnTheMap/LocationViewController.swift
+++ OnTheMap/LocationViewController.swift
@@ -17,6 +17,10 @@ class LocationViewController: UIViewController, UITextFieldDelegate {
     let labelInformation = UILabel()
     let labelLocation = UILabel()
     let labelPlease = UILabel()
+    var wide = false
+    let topLabelWidth = CGFloat(150.0)
+    let middleLabelWidth = CGFloat(75.0)
+    let bottomLabelWidth = CGFloat(100.0)
 
     
     // MARK: Outlets
@@ -73,9 +77,19 @@ class LocationViewController: UIViewController, UITextFieldDelegate {
     }
     
     override func viewWillAppear(animated: Bool) {
-        setTextLabelsForUI()
+        let screenSize: CGSize = view.frame.size
+        evaluateWide(screenSize)
+        setTextLabelsForUI(screenSize)
         
     }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+
+        evaluateWide(size)
+        
+        setTextLabelsForUI(size)
+    }
+    
     
     // MARK: User Actions
     
@@ -101,29 +115,51 @@ class LocationViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Utilities functions
     
-    func setTextLabelsForUI() {
+    func evaluateWide(size: CGSize) {
+        wide = size.width > topLabelWidth + middleLabelWidth + bottomLabelWidth
+    }
+    
+    func setTextLabelsForUI(size: CGSize) {
  
-        var wide = false
 
         // Setting the labels
-        let screenSize: CGRect = UIScreen.mainScreen().bounds
-        let screenHalfWidth = screenSize.width / 2
-        let labelWidth = CGFloat(150.0)
+//        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenHalfWidth = size.width / 2
+        
+//        wide = screenSize.width > 3 * labelWidth
         
         if wide {
+            // Landscape mode
+            let totalWidth = topLabelWidth + middleLabelWidth + bottomLabelWidth
+            let topOrigin = screenHalfWidth - totalWidth / 2.0
+            labelLocation.frame = CGRectMake(topOrigin + topLabelWidth, 65, middleLabelWidth, 25)
+            labelLocation.textAlignment = .Center
+            labelLocation.font = UIFont.boldSystemFontOfSize(18)
+            labelLocation.text = "location"
             
+            labelPlease.frame = CGRectMake(topOrigin, 65, topLabelWidth, 25)
+            labelPlease.textAlignment = .Right
+            labelPlease.font = UIFont.systemFontOfSize(18, weight: UIFontWeightThin)
+            labelPlease.text = "Please enter your"
+            
+            labelInformation.frame = CGRectMake(topOrigin + topLabelWidth + middleLabelWidth, 65, bottomLabelWidth, 25)
+            labelInformation.textAlignment = .Left
+            labelInformation.font =  UIFont.systemFontOfSize(18, weight: UIFontWeightThin)
+            labelInformation.text = "information"
+           
         } else {
-            labelPlease.frame = CGRectMake(screenHalfWidth - labelWidth / 2.0, 43, labelWidth, 25)
+            // portrait mode (on smaller phones)
+            labelPlease.frame = CGRectMake(screenHalfWidth - topLabelWidth / 2.0, 43, topLabelWidth, 25)
             labelPlease.textAlignment = .Center
             labelPlease.font = UIFont.systemFontOfSize(18, weight: UIFontWeightThin)
             labelPlease.text = "Please enter your"
 
-            labelLocation.frame = CGRectMake(screenHalfWidth - labelWidth / 2.0, 65, labelWidth, 25)
+            labelLocation.frame = CGRectMake(screenHalfWidth - middleLabelWidth / 2.0, 65, middleLabelWidth, 25)
             labelLocation.textAlignment = .Center
             labelLocation.font = UIFont.boldSystemFontOfSize(18)
             labelLocation.text = "location"
  
-            labelInformation.frame = CGRectMake(screenHalfWidth - labelWidth / 2.0, 87, labelWidth, 25)
+            labelInformation.frame = CGRectMake(screenHalfWidth - bottomLabelWidth / 2.0, 87, bottomLabelWidth, 25)
             labelInformation.textAlignment = .Center
             labelInformation.font =  UIFont.systemFontOfSize(18, weight: UIFontWeightThin)
             labelInformation.text = "information"
